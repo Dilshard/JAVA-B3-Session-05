5222804f46355d35eee7de74fcdea6d1752bc10e
Dilshard <mrdilshard@gmail.com>
2021-07-01 18:02:40 +0530

    Ex-6

--- src/com/company/Book.java
+++ src/com/company/Book.java
@@ -2,4 +2,19 @@ package com.company;
 
 public class Book {
 
+    String bookTitle;
+    int noOfPages;
+    String authorName;
+    String publisher;
+
+    public void bookDetails(){
+        System.out.println("Welcome to Book fare");
+        System.out.println("***********************");
+        System.out.println("Book Title : "+bookTitle);
+        System.out.println("No Of Pages : "+noOfPages);
+        System.out.println("Author Name : "+authorName);
+        System.out.println("Publisher: "+publisher);
+        System.out.println();
+    }
+
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,18 +4,31 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int newTotal = total(45,45) + 100 / 2 * 10;
-        System.out.println(newTotal);
-    }
+        Book book1 = new Book();
 
-    public static int total(int mk1, int mk2){
-        int total = mk1 + mk2;
-        return total;
-    }
+        book1.bookTitle = "Learn JAVA";
+        book1.noOfPages = 440;
+        book1.authorName = "Arun";
+        book1.publisher = "M.D Gunasena";
+        book1.bookDetails();
 
-    public static void total2(int mk1, int mk2){
-        int total = mk1 + mk2;
-        System.out.println(total);
-    }
+        Book book2 = new Book();
 
+        book2.bookTitle = "Zero to Hero in JAVA";
+        book2.noOfPages = 500;
+        book2.authorName = "Dhayan Vijayakumar";
+        book2.publisher = "Sarasavi Book shop";
+        book2.bookDetails();
+
+        Book book3 = new Book();
+
+        book3.bookTitle = "OOP in JAVA";
+        book3.noOfPages = 685;
+        book3.authorName = "Asky Mohamed";
+        book3.publisher = "Auston Book shop";
+        book3.bookDetails();
+
+
+
+    }
 }
