1e7b7327556b4ef372291a66d25283b57814a5c1
Dilshard <mrdilshard@gmail.com>
2021-07-01 17:23:25 +0530

    Ex-3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,16 +3,48 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        bookTitle("Learn JAVA");
+
+        studentPersonal("Arun", "arun@bitech.lk", "0741547524");
+        studentMarks(88,99,100);
+
+        studentPersonal("Asky", "asky@bitech.lk", "07715465464");
+        studentMarks(77,88,99);
+
+        studentPersonal("Dhayan", "dhayan@bitech.lk", "07154545656");
+        studentMarks(88,99,99);
+
     }
 
-    public static void bookTitle(String bookName){
+    public static void studentPersonal(String stname, String stemail, String stContact){
 
-        System.out.println("Welcome to the Book Shop!");
-        System.out.println("Book title :"+bookName);
-        System.out.println("Thank you for using our system!");
+        String studentName = stname;
+        String studentEmail = stemail;
+        String studentContact = stContact;
+
+        System.out.println("Student Name :"+studentName);
+        System.out.println("Student Email :"+studentEmail);
+        System.out.println("Student Contact :"+studentContact);
 
     }
+
+    public static void studentMarks(int mk1, int mk2, int mk3){
+
+        int marks1 = mk1;
+        int marks2 = mk2;
+        int marks3 = mk3;
+
+        studentPerformanceReport(marks1,marks2,marks3);
+    }
+
+    public static void studentPerformanceReport(int mk1, int mk2, int mk3){
+        int total = mk1 + mk2 + mk3;
+        double avg = total / 3;
+
+        System.out.println("Total :" + total);
+        System.out.println("Average :" + avg + "\n");
+    }
+
+
 
 
 }
